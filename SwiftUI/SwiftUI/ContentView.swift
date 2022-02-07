b2db2cf60038f5e90efe56ceed48ba23c2197cd1
Nisa Efendioğlu <nisaefendioglu0@gmail.com>
2022-02-07 23:18:57 +0300

    Design with code.

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Nisa Nur Efendioğlu on 7.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        Text("Hello, world!")
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Nisa Nur Efendioğlu on 7.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
