4183f7256e601c0a340bf27caa9ce46a2b1aaff5
Nisa Efendioğlu <nisaefendioglu0@gmail.com>
2022-02-08 21:21:04 +0300

    Segue.

---
+++ SegueApp/SegueApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SegueApp
+//
+//  Created by Nisa Nur Efendioğlu on 8.02.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var myLabelSecond: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ SegueApp/SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Nisa Nur Efendioğlu on 8.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
