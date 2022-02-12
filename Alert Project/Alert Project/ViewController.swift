e9a201d8335f2905bd011f30885147e4b5a447fd
Nisa Efendioğlu <nisaefendioglu0@gmail.com>
2022-02-12 13:43:46 +0300

    Alert.

--- Alert Project/Alert Project/ViewController.swift
+++ Alert Project/Alert Project/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
 
     @IBAction func signUpClicked(_ sender: Any) {
         
-        let alert = UIAlertController(title: "Error", message:"Username not found!", preferredStyle: UIAlertController.Style.alert)
+      /*  let alert = UIAlertController(title: "Error", message:"Username not found!", preferredStyle: UIAlertController.Style.alert)
         
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
             // button clicked.
@@ -28,9 +28,51 @@ class ViewController: UIViewController {
         
         alert.addAction(okButton)
         
+        self.present(alert, animated: true, completion: nil) */
+        
+        if(usernameText.text == ""){
+            
+          /*  let alert = UIAlertController(title: "Error", message: "Username not found!", preferredStyle: UIAlertController.Style.alert)
+            
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            
+            alert.addAction(okButton)
+            
+            //alerti ekranda göstermek, tamamlamak için.
+            self.present(alert, animated: true, completion: nil) */
+            
+            makeAlert(titleInput: "Error", messageInput: "Username not found!")
+
+        
+        }
+        
+        else if(passwordText.text == ""){
+            
+            makeAlert(titleInput: "Error", messageInput: "Password not found!")
+
+        }
+        
+        else if (passwordText.text != password2Text.text){
+        
+            makeAlert(titleInput: "Error", messageInput: "Passwords do not match!")
+
+        }
+        
+        else {
+            makeAlert(titleInput: "Success", messageInput: "User OK!")
+        }
+        
+    }
+    
+    func makeAlert(titleInput : String, messageInput: String ){
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(okButton)
+        
         self.present(alert, animated: true, completion: nil)
-        
-        
     }
     
 }
