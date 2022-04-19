c3dd4c39f4b91c00b1057a823806bcffd5c81f2a
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-04-19 21:52:27 +0300

    hide kenny

--- CatchTheKennyGame/CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/CatchTheKennyGame/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var score = 0
     var timer = Timer()
     var counter = 0
+    var kennyArray = [UIImageView]()
+    var hideTimer = Timer()
     
     //Views
     @IBOutlet weak var timeLabel: UILabel!
@@ -65,13 +67,32 @@ class ViewController: UIViewController {
         kenny7.addGestureRecognizer(recognizer7)
         kenny8.addGestureRecognizer(recognizer8)
         kenny9.addGestureRecognizer(recognizer9)
+        
+        kennyArray = [kenny1,kenny2,kenny3,kenny4,kenny5,kenny6,kenny7,kenny8,kenny9] // || kennyArray.apend(kenny1) ...
+         
+        
 
         //Timers
         counter = 10
         timeLabel.text = String(counter)
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideKenny), userInfo: nil, repeats: true)
+        
+        hideKenny()
 
+    }
+    
+    @objc func hideKenny(){
+        for kenny in kennyArray {
+            kenny.isHidden = true
+        }
+        
+        //-1 dizi olmasından kaynaklı olarak.
+        let random = Int(arc4random_uniform(UInt32(kennyArray.count - 1 )))
+        kennyArray[random].isHidden = false
+        
     }
 
     @objc func increaseScore(){
@@ -86,6 +107,11 @@ class ViewController: UIViewController {
 
         if(counter == 0){
             timer.invalidate()
+            hideTimer.invalidate()
+            
+            for kenny in kennyArray {
+                kenny.isHidden = true
+            }
             
             //Alert
             let alert = UIAlertController(title: "Time's Up!", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
