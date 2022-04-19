cd8f3b618ee598e59a62f0b8c3acecee8542fe75
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-04-19 20:36:51 +0300

    Catch The Kenny Game

---
+++ CatchTheKennyGame/Shared/CatchTheKennyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennyGameApp.swift
+//  Shared
+//
+//  Created by Nisa on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheKennyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
