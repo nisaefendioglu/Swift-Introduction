fa2001a3eda72cc1f5f81346545969ac30472f16
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-06 19:03:09 +0300

    created interface

--- WorldTraveler/WorldTraveler/ContentView.swift
+++ WorldTraveler/WorldTraveler/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var fahrenheitValue: String = ""
+    
+    let numberFormatter: NumberFormatter = {
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 2
+        
+        return numberFormatter
+        
+    }()
+    
+    func convertToCelsius () -> String{
+        
+        if let value = Double(fahrenheitValue){
+            let fahrenheit = Measurement<UnitTemperature>(value: value, unit: .fahrenheit)
+            
+            let celsiusValue = fahrenheit.converted(to: .celsius)
+            
+            return numberFormatter.string(from: NSNumber(value: celsiusValue.value)) ?? "?"
+        }
+        else {
+            return "?"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextField("value", text: $fahrenheitValue)
+                .keyboardType(.decimalPad)
+                .font(Font.system(size: 64))
+                .multilineTextAlignment(.center)
+            Text("Fahrenheit")
+            Text("is Actually").foregroundColor(.gray)
+            Text(convertToCelsius())
+                .font(Font.system(size: 64))
+            Text("Degress Celsius")
+            Spacer()
+        }.font(.title)
+            .foregroundColor(.orange)
     }
 }
 
