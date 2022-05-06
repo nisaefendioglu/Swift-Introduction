43a92567b7cb7a3608e144b9dc7f000f632d5bbb
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-06 18:29:23 +0300

    added world traveler project

---
+++ WorldTraveler/WorldTraveler/WorldTravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelerApp.swift
+//  WorldTraveler
+//
+//  Created by Nisa on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
