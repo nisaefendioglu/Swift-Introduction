91ddf2b8428650cdfb3da382f5116039782348da
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-08 19:34:56 +0300

    add UsedGame

---
+++ UsedGames/UsedGames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsedGames
+//
+//  Created by Nisa on 8.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsedGames/UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Nisa on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
